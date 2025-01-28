b2f11fedd7a65f1b5cdea5cb9a5282f8e4d70919
liya <liya@mac.home>
2025-01-28 09:47:09 +0000

    Liya first commit

--- Exercise_10.py
+++ Exercise_10.py
@@ -14,3 +14,16 @@ print(pattern)
 # TODO: Use the glob.glob() function to obtain the list of filenames
 list_fileName = glob.glob(pattern)
 print(list_fileName)
+
+# TODO: use os.path.getsize to find each file's size
+for file in file_names:
+    # Look for size
+    size = os.path.getsize(file)
+#     Print result
+print(file, size, bytes)
+
+# TODO: Add a test to only display files that are not zero length - this task is doing what the above task should do
+for file in file_names:
+ # condition for file lengths greater than 0
+ if size > 0:
+     print(file, size, 'bytes')
