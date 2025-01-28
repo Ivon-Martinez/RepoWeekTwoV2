20b489af675a8b2c2f32b0c5acafcbae62fcc99f
Daliya Stanley <daliya.kurien@gmail.com>
2025-01-28 12:37:26 +0000

    EDIT: added getpass

---
+++ pin.py
@@ -0,0 +1,21 @@
+import getpass
+
+
+original_pin = 1234
+# supplied_pin = input("Enter your PIN: ")
+attempts = 3
+count = 0
+
+while count < attempts:
+    supplied_pin = getpass.getpass(prompt="Enter your PIN: ")
+    if original_pin == supplied_pin:
+        print("Its the right pin")
+    else:
+        count += 1
+        if attempts - count == 0:
+            print("sorry you are logged out!")
+            break
+        print("Wrong PIN! You have" +" "+ str(attempts-count) + "attempts left."
+
+
+
